5229eb6795b174ed3388262da59963707ac3385d
Moritz Schöfl <moritz.schoefl@datavisyn.io>
2021-10-28 15:07:22 +0200

    added mui styles dep

--- src/Overrides/CimeAppBar.tsx
+++ src/Overrides/CimeAppBar.tsx
@@ -19,7 +19,7 @@ export function CimeAppBar() {
           variant="h6"
           style={{ marginLeft: 48, color: "rgba(0, 0, 0, 0.54)" }}
         >
-          {"CIME: Chem-Informatics Model Explorer"}
+          {"CIME: ChemInformatics Model Explorer"}
         </Typography>
       </Toolbar>
     </AppBar>
